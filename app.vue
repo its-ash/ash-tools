83a85a8ac6fb849e348bc0d88f99e6ca3acca304
ashvinijangid <ashvinijangid@gmail.com>
2026-02-25 13:37:29 +0530

    Added clear console

--- app.vue
+++ app.vue
@@ -3,3 +3,31 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+
+
+<script setup>
+
+
+function print() {
+  let hue = Math.random() * 360;
+  setTimeout(console.clear, 0);
+  setTimeout(
+    console.log.bind(
+      console,
+      '%c✨ Welcome to Ash Tools ✨',
+      `color: hsl(${hue}, 100%, 50%); font-size: 5vw; font-weight: bold;`
+    )
+  );
+}
+
+
+if (!process.dev) {
+  print();
+  setTimeout(print, 1000)
+}
+
+
+
+
+</script>
